bdf5c128f1676f743ca0ea8a70169971b14cdad1
Reisson Ramos Silva <58003762+Reisson@users.noreply.github.com>
2019-12-30 14:26:19 -0300

    Create Conta3_5.java
    Como declarar uma classe.
    Como criar um objeto dentro da classe.

---
+++ Conta3_5.java
@@ -0,0 +1,24 @@
+// Figura3.5 Conta.java
+// A Classe Conta com um construtor que inicializa o nome.
+// Data 30/12/2019
+// Autor reissonramos@gmail.com
+
+public class Conta3_5
+{
+	private String name;
+	
+	public Conta3_5(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+}
