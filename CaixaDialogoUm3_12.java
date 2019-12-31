260fbac22ed8eb9def4db389fdf32bd8e1e1c903
Reisson Ramos Silva <58003762+Reisson@users.noreply.github.com>
2019-12-31 11:22:50 -0300

    Add files via upload

---
+++ CaixaDialogoUm3_12.java
@@ -0,0 +1,14 @@
+// Figura 3.12: CaixaDialogoUm3_12
+// Usando o JOptionPane para exibir múltiplas linhas em uma caixa de mensagem
+// Data 31/12/2019
+// Autor reissonramos@gmail.com
+import javax.swing.JOptionPane;
+
+public class CaixaDialogoUm3_12
+{
+	public static void main(String[] args)
+	{
+		//Exibir a mensagem em uma caixa de dialogo.
+		JOptionPane.showMessageDialog(null,"Bem vindo ao Java");
+	}
+}
