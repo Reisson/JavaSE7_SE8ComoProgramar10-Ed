e4d773a8d94c8ce9064455f113d34570390be692
Reisson Ramos Silva <58003762+Reisson@users.noreply.github.com>
2020-01-03 15:51:11 -0300

    Operador condicional  ( ?: )
    Opetador ternário
    Expreção condicional
    System.out.println( variavel  >=60 ? "Aprovado" : "Failed");

---
+++ TesteOpCond.java
@@ -0,0 +1,13 @@
+//Testar a função operador condicional(?:) 
+//Data 03/01/2019
+//Autor reissonramos@gmail.com
+
+public class TesteOpCond //inicio da classe Obs(o nome da class deve ser o mesmo nome do aquivo salvo na exteção .java).
+{
+	public static void main(String[] args)//inicio metodo main
+	{
+		int teste; //declarando variavel
+		teste = 60; // atribuindo valor a variavel
+		System.out.println(teste >=60 ? "Aprovado" : "Reprovado");// |teste condicão| ? |Cond Verdverdadeira|  : |Cond Falsa|
+	} //fim do metodo main
+} //Fim da class
